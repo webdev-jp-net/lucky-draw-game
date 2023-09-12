3ba152a6980cc35b9cd6dab5755c27eed63e8b90
webdev <haitani@tam-tam.co.jp>
2023-09-12 18:52:24 +0900

    Add: firestoreとの連携をRTK Queryで管理

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
-import user from './user';
+import user, { statusApi } from './user';
 
 const reducer = combineReducers({
   user,
+  [statusApi.reducerPath]: statusApi.reducer,
 });
 
-const middleware = getDefaultMiddleware({ serializableCheck: false });
+const middleware = getDefaultMiddleware({ serializableCheck: false }).concat(statusApi.middleware);
+
 export const store = configureStore({ reducer, middleware });
 
 export type RootState = ReturnType<typeof reducer>;
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,12 +1,56 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from 'firebaseDB';
 
 type State = {
   userId: string;
+  memberList: string[];
+  drawResult?: string;
 };
 
 const initialState: State = {
   userId: '',
+  memberList: [],
 };
+
+type EntriesResponse = {
+  memberList: string[];
+};
+
+type DrawResultResponse = {
+  userId?: string;
+};
+
+// RTK Queryの設定
+// https://redux-toolkit.js.org/rtk-query/overview
+type FirebaseBaseQueryParams = {
+  path: string;
+};
+const firebaseBaseQuery = async ({ path }: FirebaseBaseQueryParams) => {
+  const docRef = doc(db, 'status', path);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    return { data: docSnap.data() };
+  } else {
+    return { error: 'No such document!' };
+  }
+};
+
+export const statusApi = createApi({
+  reducerPath: 'statusApi',
+  baseQuery: firebaseBaseQuery,
+  endpoints: builder => ({
+    getEntries: builder.query<EntriesResponse, void>({
+      query: () => ({ path: 'room_entries' }),
+    }),
+    getDrawResult: builder.query<DrawResultResponse, void>({
+      query: () => ({ path: 'draw_result' }),
+    }),
+  }),
+});
+
+export const { useGetEntriesQuery, useGetDrawResultQuery } = statusApi;
 
 const user = createSlice({
   name: 'user',
@@ -17,6 +61,21 @@ const user = createSlice({
     updateUserId: (state, action) => {
       state.userId = action.payload;
     },
+  },
+
+  extraReducers: builder => {
+    builder.addMatcher(
+      statusApi.endpoints.getEntries.matchFulfilled,
+      (state, action: PayloadAction<EntriesResponse>) => {
+        state.memberList = action.payload.memberList;
+      }
+    );
+    builder.addMatcher(
+      statusApi.endpoints.getDrawResult.matchFulfilled,
+      (state, action: PayloadAction<DrawResultResponse>) => {
+        state.drawResult = action.payload.userId;
+      }
+    );
   },
 });
 
